2a31238e9bfaa842bddb4e33fbdcbd14710fc873
clinse <christoph.linse@prodyna.com>
2020-01-30 17:29:03 +0100

    analysing functions removed, workflow adapted, implementation improved

--- chapters/main/practical/listings/code/fetchdirect.py
+++
@@ -1,9 +0,0 @@
-#   set targets for ADD and COPY
-def fetch_direct_copy_targets(self):
-    temp_list = list()
-    for match in re.finditer("(dir|file):[a-f0-9]{64}\sin\s", self.img_meta):
-        temp_list.append(self.img_meta[match.span()[1]:self.img_meta.find("'", match.span()[1]) - 1])
-
-    # has to be removed because of trusted upper base layer
-    temp_list.remove('/')
-    return temp_list
